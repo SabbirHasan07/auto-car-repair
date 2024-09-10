beed63e50584513c372b835d186a3c88d9b9d44d
SabbirHasan07 <sabbirhasan.engr@gmail.com>
2024-09-10 11:32:38 +0600

    update

--- app/Components/Home/CompanyServices.tsx
+++ app/Components/Home/CompanyServices.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
+
 import Link from 'next/link';
 import React from 'react';
-
+import Image from 'next/image';
 const CompanyServices: React.FC = () => {
     return (
         <div>
@@ -11,10 +11,10 @@ const CompanyServices: React.FC = () => {
                     <Link href="#">
                         <Image
                             className="rounded-t-lg"
-                            src="/docs/images/blog/image-1.jpg"
+                            src="/assets/log.jpg"
                             alt="Technology Acquisitions 2021"
-                            height={50}
-                            width={50}
+                            height={500}
+                            width={500}
 
                         />
                     </Link>
@@ -54,10 +54,10 @@ const CompanyServices: React.FC = () => {
                     <Link href="#">
                         <Image
                             className="rounded-t-lg"
-                            src="/docs/images/blog/image-1.jpg"
+                            src="/assets/mechanical.jpeg"
                             alt="Technology Acquisitions 2021"
-                            height={50}
-                            width={50}
+                            height={500}
+                            width={500}
                         />
                     </Link>
                     <div className="p-5">
@@ -96,10 +96,10 @@ const CompanyServices: React.FC = () => {
                     <Link href="#">
                         <Image
                             className="rounded-t-lg"
-                            src="/docs/images/blog/image-1.jpg"
+                            src="/assets/images (1).jpeg"
                             alt="Technology Acquisitions 2021"
-                            height={50}
-                            width={50}
+                            height={500}
+                            width={500}
                         />
                     </Link>
                     <div className="p-5">
@@ -147,11 +147,11 @@ const CompanyServices: React.FC = () => {
                     <div className="p-5">
                         <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                Noteworthy technology acquisitions 2021
+                                DIAGNOSTICS
                             </h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            Qualified mechanics conduct a full range of mechanical repairs, replacements, and diagnostics for all types of vehicles.
+                            Complete electronic diagnostic services to identify faults and provide solutions.
                         </p>
                         <Link
                             href="#"
@@ -189,11 +189,11 @@ const CompanyServices: React.FC = () => {
                     <div className="p-5">
                         <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                Noteworthy technology acquisitions 2021
+                                SUSPENSION
                             </h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            We provide complete car servicing, including logbook servicing, using the latest equipment and high-quality parts.
+                            Our team provides complete suspension inspection, service, and repair services.
                         </p>
                         <Link
                             href="#"
@@ -231,12 +231,145 @@ const CompanyServices: React.FC = () => {
                     <div className="p-5">
                         <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                Noteworthy technology acquisitions 2021
+                                BATTERIES
                             </h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            We provide complete car servicing, including logbook servicing, using the latest equipment and high-quality parts.
+                            Keep your car running with AutoPlus battery replacement and testing services.
                         </p>
+
+                        <Link
+                            href="#"
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Read more
+                            <svg
+                                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 14 10"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                                />
+                            </svg>
+                        </Link>
+                    </div>
+                </div>
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <Link href="#">
+                        <Image
+                            className="rounded-t-lg"
+                            src="/docs/images/blog/image-1.jpg"
+                            alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
+                        />
+                    </Link>
+                    <div className="p-5">
+                        <Link href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                FUEL INJECTION SERVICES
+                            </h5>
+                        </Link>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            Regular EFI servicing helps your car operate at its optimum fuel efficiency and power.
+                        </p>
+
+
+                        <Link
+                            href="#"
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Read more
+                            <svg
+                                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 14 10"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                                />
+                            </svg>
+                        </Link>
+                    </div>
+                </div>
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <Link href="#">
+                        <Image
+                            className="rounded-t-lg"
+                            src="/docs/images/blog/image-1.jpg"
+                            alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
+                        />
+                    </Link>
+                    <div className="p-5">
+                        <Link href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                COOLING SYSTEM
+                            </h5>
+                        </Link>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            A properly operating cooling system helps keep your engine running at its best.
+                        </p>
+
+
+                        <Link
+                            href="#"
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Read more
+                            <svg
+                                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 14 10"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                                />
+                            </svg>
+                        </Link>
+                    </div>
+                </div>
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <Link href="#">
+                        <Image
+                            className="rounded-t-lg"
+                            src="/docs/images/blog/image-1.jpg"
+                            alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
+                        />
+                    </Link>
+                    <div className="p-5">
+                        <Link href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                TYRES
+                            </h5>
+                        </Link>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            Selected stores now stock and fit a range of quality tyres at competitive prices.
+                        </p>
+
+
                         <Link
                             href="#"
                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
