1ebf0e4338accafa8b3a6650e12aec1cd786e98d
SabbirHasan07 <sabbirhasan.engr@gmail.com>
2024-09-10 10:48:45 +0600

    10/9/2024-update

--- app/Components/Home/CompanyServices.tsx
+++ app/Components/Home/CompanyServices.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const CompanyServices: React.FC = () => {
@@ -6,24 +8,26 @@ const CompanyServices: React.FC = () => {
             <div><h1 className='py-11 text-center font-bold text-4xl'>Services</h1></div>
             <div className='grid grid-cols-3 gap-4 p-4 items-center text-center justify-center'>
                 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img
+                    <Link href="#">
+                        <Image
                             className="rounded-t-lg"
                             src="/docs/images/blog/image-1.jpg"
                             alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
+
                         />
-                    </a>
+                    </Link>
                     <div className="p-5">
-                        <a href="#">
+                        <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                 LOG BOOK SERVICE
                             </h5>
-                        </a>
+                        </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            All car servicing, including log book servicing. We use the latest equipment and quality parts.
-
+                            We provide complete car servicing, including logbook servicing, using the latest equipment and high-quality parts.
                         </p>
-                        <a
+                        <Link
                             href="#"
                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
@@ -43,27 +47,29 @@ const CompanyServices: React.FC = () => {
                                     d="M1 5h12m0 0L9 1m4 4L9 9"
                                 />
                             </svg>
-                        </a>
+                        </Link>
                     </div>
                 </div>
                 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img
+                    <Link href="#">
+                        <Image
                             className="rounded-t-lg"
                             src="/docs/images/blog/image-1.jpg"
                             alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
                         />
-                    </a>
+                    </Link>
                     <div className="p-5">
-                        <a href="#">
+                        <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                 MECHANICAL REPAIRS
                             </h5>
-                        </a>
+                        </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            Qualified mechanics conduct a full range of mechanical repairs, replacement and diagnostics.
+                            Qualified mechanics conduct a full range of mechanical repairs, replacements, and diagnostics for all types of vehicles.
                         </p>
-                        <a
+                        <Link
                             href="#"
                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
@@ -83,27 +89,29 @@ const CompanyServices: React.FC = () => {
                                     d="M1 5h12m0 0L9 1m4 4L9 9"
                                 />
                             </svg>
-                        </a>
+                        </Link>
                     </div>
                 </div>
                 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img
+                    <Link href="#">
+                        <Image
                             className="rounded-t-lg"
                             src="/docs/images/blog/image-1.jpg"
                             alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
                         />
-                    </a>
+                    </Link>
                     <div className="p-5">
-                        <a href="#">
+                        <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                 BREAK AND CLUTCH
                             </h5>
-                        </a>
+                        </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            Brake & clutch service and repairs, from brake pads to clutch kits, we've got you covered.
+                            Brake &amp; clutch service and repairs, from brake pads to clutch kits, we&apos;ve got you covered.
                         </p>
-                        <a
+                        <Link
                             href="#"
                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
@@ -123,27 +131,29 @@ const CompanyServices: React.FC = () => {
                                     d="M1 5h12m0 0L9 1m4 4L9 9"
                                 />
                             </svg>
-                        </a>
+                        </Link>
                     </div>
                 </div>
                 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img
+                    <Link href="#">
+                        <Image
                             className="rounded-t-lg"
                             src="/docs/images/blog/image-1.jpg"
                             alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
                         />
-                    </a>
+                    </Link>
                     <div className="p-5">
-                        <a href="#">
+                        <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                 Noteworthy technology acquisitions 2021
                             </h5>
-                        </a>
+                        </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                            Qualified mechanics conduct a full range of mechanical repairs, replacements, and diagnostics for all types of vehicles.
                         </p>
-                        <a
+                        <Link
                             href="#"
                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
@@ -163,27 +173,29 @@ const CompanyServices: React.FC = () => {
                                     d="M1 5h12m0 0L9 1m4 4L9 9"
                                 />
                             </svg>
-                        </a>
+                        </Link>
                     </div>
                 </div>
                 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img
+                    <Link href="#">
+                        <Image
                             className="rounded-t-lg"
                             src="/docs/images/blog/image-1.jpg"
                             alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
                         />
-                    </a>
+                    </Link>
                     <div className="p-5">
-                        <a href="#">
+                        <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                 Noteworthy technology acquisitions 2021
                             </h5>
-                        </a>
+                        </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                            We provide complete car servicing, including logbook servicing, using the latest equipment and high-quality parts.
                         </p>
-                        <a
+                        <Link
                             href="#"
                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
@@ -203,27 +215,29 @@ const CompanyServices: React.FC = () => {
                                     d="M1 5h12m0 0L9 1m4 4L9 9"
                                 />
                             </svg>
-                        </a>
+                        </Link>
                     </div>
                 </div>
                 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img
+                    <Link href="#">
+                        <Image
                             className="rounded-t-lg"
                             src="/docs/images/blog/image-1.jpg"
                             alt="Technology Acquisitions 2021"
+                            height={50}
+                            width={50}
                         />
-                    </a>
+                    </Link>
                     <div className="p-5">
-                        <a href="#">
+                        <Link href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                 Noteworthy technology acquisitions 2021
                             </h5>
-                        </a>
+                        </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                            We provide complete car servicing, including logbook servicing, using the latest equipment and high-quality parts.
                         </p>
-                        <a
+                        <Link
                             href="#"
                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
@@ -243,7 +257,7 @@ const CompanyServices: React.FC = () => {
                                     d="M1 5h12m0 0L9 1m4 4L9 9"
                                 />
                             </svg>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
--- app/Components/Home/WelcomeText.tsx
+++ app/Components/Home/WelcomeText.tsx
@@ -7,35 +7,45 @@ const WelcomeText = () => {
             <div className="lg:flex grid gap-4 p-4">
                 <div className="grid grid-cols-3 gap-4">
                     <div>
-                        <img
+                        <Image
+                            height={50}
+                            width={50}
                             className="h-auto max-w-full rounded-lg"
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
                             alt="Gallery Image 1"
                         />
                     </div>
                     <div>
-                        <img
+                        <Image
+                            height={50}
+                            width={50}
                             className="h-auto max-w-full rounded-lg"
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
                             alt="Gallery Image 2"
                         />
                     </div>
                     <div>
-                        <img
+                        <Image
+                            height={50}
+                            width={50}
                             className="h-auto max-w-full rounded-lg"
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
                             alt="Gallery Image 3"
                         />
                     </div>
                     <div>
-                        <img
+                        <Image
+                            height={50}
+                            width={50}
                             className="h-auto max-w-full rounded-lg"
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
                             alt="Gallery Image 4"
                         />
                     </div>
                     <div>
-                        <img
+                        <Image
+                            height={50}
+                            width={50}
                             className="h-auto max-w-full rounded-lg"
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
                             alt="Gallery Image 5"
@@ -43,8 +53,8 @@ const WelcomeText = () => {
                     </div>
                 </div>
                 <div>
-                    <p className="text-xl ">
-                        "Welcome to Car Repair, your trusted destination for all your car repair and maintenance needs! Whether you're dealing with routine tune-ups, complex engine diagnostics, or unexpected breakdowns, our skilled technicians are here to provide top-quality service with honesty and efficiency. We understand how essential your vehicle is to your daily life, and our goal is to keep you safely on the road with reliable repairs and expert care. With years of experience and a commitment to customer satisfaction, CARRE PAIR ensures that your car is in the best hands. Visit us today for fast, dependable, and friendly service!"
+                    <p className="text-xl">
+                        &quot;Welcome to Car Repair, your trusted destination for all your car repair and maintenance needs! Whether you&apos;re dealing with routine tune-ups, complex engine diagnostics, or unexpected breakdowns, our skilled technicians are here to provide top-quality service with honesty and efficiency. We understand how essential your vehicle is to your daily life, and our goal is to keep you safely on the road with reliable repairs and expert care. With years of experience and a commitment to customer satisfaction, CARRE PAIR ensures that your car is in the best hands. Visit us today for fast, dependable, and friendly service!&quot;
                     </p>
                 </div>
 
